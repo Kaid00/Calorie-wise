8f9eb0984baeb930582a058165c1ab7b8e8a2c05
Azamah Jr <depixellife@gmail.com>
2024-02-26 04:18:46 +0000

    Initial commit

---
+++ test/cw_backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CwBackend from '../lib/cw_backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cw_backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CwBackend.CwBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
