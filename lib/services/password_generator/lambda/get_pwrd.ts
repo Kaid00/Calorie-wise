65a1bd05cb0982fc637dd3c9aa14183822ba91c5
Azamah Jr <depixellife@gmail.com>
2024-03-08 11:22:06 +0000

    test with params

--- lib/services/password_generator/lambda/get_pwrd.ts
+++ lib/services/password_generator/lambda/get_pwrd.ts
@@ -5,12 +5,31 @@ import { genPassword } from "./utils/gen_pwrd";
 export async function getPassword(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
 
     if(event.queryStringParameters) {
-        const special = event.queryStringParameters['special']
-        const number = event.queryStringParameters['number']
-        const upper = event.queryStringParameters['upper']
-        const length = event.queryStringParameters['length']
+        let special: Boolean = true
+        let number: Boolean = true
+        let upper: Boolean = true
+        if (event.queryStringParameters['special'] == 'off') {
+            special = false
+        }
 
+        if (event.queryStringParameters['number'] == 'off') {
+            number = false
+        }
+        if (event.queryStringParameters['upper'] == 'off') {
+            upper = false
+        }
 
+        const length: number = event.queryStringParameters['length'] as unknown as number
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                status: 'successful and has params',
+                password: genPassword(number, special, length, upper),
+                params: `special: ${event.queryStringParameters['special']} \n number: ${event.queryStringParameters['number']}, upper: ${event.queryStringParameters['upper']}, length: ${event.queryStringParameters['length']}`
+            })
+        }
+    
     } 
 
     // const result = genPassword()
